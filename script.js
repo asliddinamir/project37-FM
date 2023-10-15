442b887a99e62ba1138311b22771d596212bb632
Asliddin Amirov <asliddinamir2@gmail.com>
2023-10-15 22:54:33 +0800

    last commit

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-const menu = document.querySelector('.menu')
-const navbar = document.querySelector('.navbar')
+const menu = document.querySelector('.menu');
+const closeBtn = document.querySelector('.close');
+const navbar = document.querySelector('.navbar');
 
-menu.addEventListener('click', () => {
-    navbar.classList.toggle('show-nav')
-})
+menu.addEventListener('click', toggleNavbar);
+
+closeBtn.addEventListener('click', toggleNavbar);
+
+function toggleNavbar() {
+    if (navbar.classList.contains('show-nav')) {
+        navbar.classList.remove('show-nav');
+        menu.style.display = 'block';
+        closeBtn.style.display = 'none';
+    } else {
+        navbar.classList.add('show-nav');
+        menu.style.display = 'none';
+        closeBtn.style.display = 'block';
+    }
+}
